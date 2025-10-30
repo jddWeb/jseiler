3ac2541c22103d6613084b061553a2a835c17202
Jens Seiler <buero@jdd-webdesign.de>
2025-10-30 14:05:55 +0100

    TsConfig erstellt

--- packages/jssitepackage/ext_localconf.php
+++ packages/jssitepackage/ext_localconf.php
@@ -2,5 +2,18 @@
 
 defined('TYPO3') or die('Access denied.');
 
-// Add default RTE configuration
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['jssitepackage'] = 'EXT:jssitepackage/Configuration/RTE/Default.yaml';
+call_user_func(function () {
+    // Page TSconfig
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        "@import 'EXT:jssitepackage/Configuration/TsConfig/Page/Page.tsconfig'"
+    );
+
+    // User TSconfig
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+        "@import 'EXT:jssitepackage/Configuration/TsConfig/User/User.tsconfig'"
+    );
+
+    // RTE preset
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['jssitepackage']
+        = 'EXT:jssitepackage/Configuration/RTE/Default.yaml';
+});
