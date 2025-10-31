beab0a37b4dc1234aca95f27b31dca8d88f449a2
Jens Seiler <buero@jdd-webdesign.de>
2025-10-31 10:00:41 +0100

    php-Fehler behoben - Text und Menü wird nicht ausgegeben

--- packages/jssitepackage/ext_localconf.php
+++ packages/jssitepackage/ext_localconf.php
@@ -1,19 +1,14 @@
 <?php
+defined('TYPO3') or die();
 
-defined('TYPO3') or die('Access denied.');
-
-call_user_func(function () {
-    // Page TSconfig
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        "@import 'EXT:jssitepackage/Configuration/TsConfig/Page/Page.tsconfig'"
-    );
-
-    // User TSconfig
+call_user_func(static function () {
+    // Übergangsweise nötig bis TYPO3 14: UserTSconfig manuell einbinden
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
         "@import 'EXT:jssitepackage/Configuration/TsConfig/User/User.tsconfig'"
     );
 
-    // RTE preset
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['jssitepackage']
-        = 'EXT:jssitepackage/Configuration/RTE/Default.yaml';
+    // PageTSconfig für BackendLayouts (Übergangsphase)
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        "@import 'EXT:jssitepackage/Configuration/TsConfig/Page/page.tsconfig'"
+    );
 });
